02e127b437dafaf61fb2332e1157ab54c626305c
dinarBerkan <berkandinar@gmail.com>
2025-03-14 21:00:05 +0300

    Creating initial project and adding it to GitHub

---
+++ src/test/java/com/challenge/brokagefirmapp/BrokageFirmAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.brokagefirmapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrokageFirmAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
